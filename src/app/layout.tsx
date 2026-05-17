2c1988193884a56572589a1cecf01b6d311a21df
Ollan Muza <amuza@piermanagement.com>
2026-05-17 17:32:10 +0100

    To remove hydration issue --- needs to be address!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} bg-default min-h-screen text-base`}>
         <Script id="theme-init" strategy="beforeInteractive">
           {`
